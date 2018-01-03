e8e43bf4efbea8ea68a8481f1948b596fc5bf0af
eugene <eugene.goldberg117@gmail.com>
2018-01-03 16:08:15 -0600

    initial commit

---
+++ spring-boot-sample-data-jest/src/main/java/sample/data/jest/Loaders.java
@@ -0,0 +1,51 @@
+package sample.data.jest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class Loaders {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @PostConstruct
+    @Transactional
+    public void loadAll(){
+        CustomerRepository cr = repository;
+        this.repository.deleteAll();
+        saveCustomers();
+        fetchAllCustomers();
+        fetchIndividualCustomers();
+
+    }
+
+    private void saveCustomers() {
+        this.repository.save(new Customer("Jerry", "Smith"));
+        this.repository.save(new Customer("Bob", "Smith"));
+    }
+
+    private void fetchAllCustomers() {
+        System.out.println("Customers found with findAll():");
+        System.out.println("-------------------------------");
+        for (Customer customer : this.repository.findAll()) {
+            System.out.println(customer);
+        }
+        System.out.println();
+    }
+
+    private void fetchIndividualCustomers() {
+        System.out.println("Customer found with findByFirstName('Alice'):");
+        System.out.println("--------------------------------");
+        System.out.println(this.repository.findByFirstName("Alice"));
+
+        System.out.println("Customers found with findByLastName('Smith'):");
+        System.out.println("--------------------------------");
+        for (Customer customer : this.repository.findByLastName("Smith")) {
+            System.out.println(customer);
+        }
+    }
+}
